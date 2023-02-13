15c03262ca8c094dfbb03a2df2c1cdec9b49b0a3
Peter Manser <mail@petermanser.ch>
2023-02-13 15:16:19 +0100

    chore: Move restapi to .restapi

---
+++ .restapi/restapi/qwacker-openai.ts/index.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export { LikesService } from './services/LikesService';
+export { PostsService } from './services/PostsService';
+export { UsersService } from './services/UsersService';
